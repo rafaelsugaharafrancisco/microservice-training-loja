a4b968971cedc8f54e7e9010e493dc125f8f6d11
Rafael <rafa.s.francisco@gmail.com>
2022-08-13 13:25:23 -0300

    ensinando o spring a resolver a url do servidor

--- src/main/java/br/com/alura/loja/service/CompraService.java
+++ src/main/java/br/com/alura/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,13 @@ import br.com.alura.loja.dto.CompraDto;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void efetuarCompra(CompraDto compra) {
 		
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoDestinoDto> exchange = client.
-				exchange("http://localhost:8081/info/" + compra.getLogradouro().getEstado(),
+				exchange("http://fornecedor/info/" + compra.getLogradouro().getEstado(),
 				HttpMethod.GET, null, InfoDestinoDto.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
